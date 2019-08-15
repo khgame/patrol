cb078cb117f58b8eb5fa3f00a40ecfd4a8784535
bagaking <zfaustk@qq.com>
2019-08-15 16:30:07 +0800

    feat (patrolWorker): support batch task

--- src/workers/patrolWorker.ts
+++ src/workers/patrolWorker.ts
@@ -17,6 +17,7 @@ import * as fs from "fs-extra";
 import * as Path from "path";
 import {IPatrolRule} from "../const";
 import {Continuous} from "../core/continuous";
+import * as _ from "lodash";
 
 interface ISchedulerResult {
     status: "ok" | "error";
@@ -24,14 +25,17 @@ interface ISchedulerResult {
     data: any;
 }
 
-interface IScheduler {
-    tag: string;
-    hash: string;
+interface ITask {
     rule: string;
     job?: Job | Continuous;
     running: boolean;
 }
 
+interface IScheduler {
+    tag: string;
+    hash: string;
+    tasks: ITask[];
+}
 
 export class PatrolWorker extends Worker implements IWorker {
 
@@ -61,47 +65,29 @@ export class PatrolWorker extends Worker implements IWorker {
             throw new Error(`⊙ remove scheduler failed: the scheduler ${tag} are not exist.`);
         }
 
-        if (scheduler.job) {
-            scheduler.job.cancel();
-        }
+        scheduler.tasks.forEach(t => t.job && t.job.cancel());
 
         this.schedulers.splice(this.schedulers.indexOf(scheduler), 1);
     }
 
-    public insertScheduler(
+    public insertTask(
         tag: string,
-        hash: string,
+        ind: string,
         rule: string,
         method: (log: Logger) => Promise<void>
-    ) {
-        const scheduler = this.getScheduler(tag);
-
-        if (scheduler) {
-            if (scheduler.hash === hash) {
-                throw new Error(`⊙ insert scheduler failed: the scheduler ${tag} of hash ${hash} is already exist.`);
-            }
-
-            if (scheduler.running) {
-                this.log.info(`⊙ replace scheduler ${tag} of hash ${scheduler.hash} failed: the scheduler ${tag} of hash ${hash} is running.`);
-                return;
-            }
-
-            this.log.info(`⊙ detect scheduler ${tag} hash changed, ${scheduler.hash} => ${hash}, start update.`);
-            this.removeScheduler(tag);
-        }
-
-        const task: IScheduler = {
-            tag, rule, hash, running: false
+    ): ITask {
+        const task: ITask = {
+            rule, running: false
         };
 
         const procedure = async () => {
             this.processRunning += 1;
             task.running = true;
             try {
-                this.log.warn(`⊙ schedule ${tag} triggered, rule:"${rule}"`);
+                this.log.warn(`⊙ schedule ${tag}:${ind} triggered, rule:"${rule}"`);
                 await Promise.resolve(method(genLogger(tag)));
             } catch (e) {
-                this.log.error(`⊙ schedule ${tag} exited, rule:"${rule}" error: ${e}, ${e.stack} `);
+                this.log.error(`⊙ schedule ${tag}:${ind} exited, rule:"${rule}" error: ${e}, ${e.stack} `);
                 throw e;
             } finally {
                 this.processRunning -= 1;
@@ -116,7 +102,61 @@ export class PatrolWorker extends Worker implements IWorker {
         this.log.info(`⊙ created job ${tag} rule:"${rule}" job:${job}`);
 
         task.job = job;
-        this.schedulers.push(task);
+
+        return task;
+    }
+
+
+    public insertScheduler(
+        path: string,
+        tag: string,
+        hash: string,
+        datas: Array<{ rule: string, script: string }>
+    ) {
+        if (datas.length <= 0) {
+            throw new Error(`⊙ insert scheduler failed: the input config is empty.`);
+        }
+
+        const scheduler = this.getScheduler(tag);
+
+        if (scheduler) {
+            if (scheduler.hash === hash) {
+                throw new Error(`⊙ insert scheduler failed: the scheduler ${tag} of hash ${hash} is already exist.`);
+            }
+
+            const indRunningTask = scheduler.tasks.findIndex(t => t.running);
+            if (indRunningTask >= 0) {
+                this.log.info(`⊙ replace scheduler ${tag} of hash ${scheduler.hash} failed: the task ${indRunningTask} of scheduler ${tag}(hash ${hash}) is running.`);
+                return;
+            }
+
+            this.log.info(`⊙ detect scheduler ${tag} hash changed, ${scheduler.hash} => ${hash}, start update.`);
+            this.removeScheduler(tag);
+        }
+
+        const tasks: ITask[] = [];
+        for (let ind in datas) {
+            const {rule, script} = datas[ind];
+            const requirePath = Path.isAbsolute(script) ? script : Path.resolve(Path.dirname(path), script);
+            try {
+                if (require.cache[requirePath]) {
+                    this.log.info(`⊙ clear require cache of ${requirePath}`);
+                    delete require.cache[requirePath];
+                }
+                const {patrol} = require(requirePath);
+
+                const task = this.insertTask(tag, datas.length > 1 ? ind : "-", rule, patrol);
+                tasks.push(task);
+            } catch (e) {
+                // todo
+            }
+        }
+
+        const newScheduler: IScheduler = {
+            tag, hash, tasks
+        };
+
+        this.schedulers.push(newScheduler);
 
         return true;
     }
@@ -141,12 +181,12 @@ export class PatrolWorker extends Worker implements IWorker {
 
             this.processRunning += 1;
             try {
-                let ds = [];
+                let configPathes = [];
                 const pthLocal = process.cwd();
                 const pthConf = "/etc/patrol/conf.d";
 
                 if (fs.existsSync(pthLocal)) {
-                    ds.push(
+                    configPathes.push(
                         ... fs.readdirSync(pthLocal)
                             .filter(
                                 str => str.trim().toLowerCase().endsWith(".patrol.json")
@@ -155,7 +195,7 @@ export class PatrolWorker extends Worker implements IWorker {
                 }
 
                 if (fs.existsSync(pthConf)) {
-                    ds.push(
+                    configPathes.push(
                         ... fs.readdirSync(pthConf)
                             .filter(str =>
                                 str.trim().toLowerCase().endsWith(".patrol.json")
@@ -163,15 +203,15 @@ export class PatrolWorker extends Worker implements IWorker {
                     );
                 }
 
-                for (const i in ds) {
-                    const d = ds[i];
+                for (const i in configPathes) {
+                    const confPath = configPathes[i];
 
-                    const baseName = Path.basename(d);
+                    const baseName = Path.basename(confPath);
                     const tagName = baseName.substr(0, baseName.length - 12);
 
                     try {
-                        const dataStr = fs.readFileSync(d).toString();
-                        const data = JSON.parse(dataStr);
+                        const dataStr = fs.readFileSync(confPath).toString();
+                        let data = JSON.parse(dataStr);
                         const hash = Crypto.getMd5(dataStr);
 
                         const existedScheduler = this.getScheduler(tagName);
@@ -179,19 +219,23 @@ export class PatrolWorker extends Worker implements IWorker {
                             continue;
                         }
 
-                        this.log.info(`⊙ start load scheduler ${tagName}: read file ${d}`);
+                        this.log.info(`⊙ start load scheduler ${tagName}: read file ${confPath}`);
 
-                        const requirePath = Path.isAbsolute(data.script) ? data.script : Path.resolve(Path.dirname(d), data.script);
-                        if (require.cache[requirePath]) {
-                            this.log.info(`⊙ clear require cache of ${requirePath}`);
-                            delete require.cache[requirePath];
+                        if (!_.isArray(data)) {
+                            data = [data];
                         }
-                        const {patrol} = require(requirePath);
 
-                        this.insertScheduler(tagName, hash, data.rule, patrol);
+                        // const requirePath = Path.isAbsolute(data.script) ? data.script : Path.resolve(Path.dirname(d), data.script);
+                        // if (require.cache[requirePath]) {
+                        //     this.log.info(`⊙ clear require cache of ${requirePath}`);
+                        //     delete require.cache[requirePath];
+                        // }
+                        // const {patrol} = require(requirePath);
+
+                        this.insertScheduler(confPath, tagName, hash, data);
 
                     } catch (e) {
-                        console.error(`⊙ load data ${d} error: ${e}, ${e.stack}`);
+                        console.error(`⊙ load data ${confPath} error: ${e}, ${e.stack}`);
                     }
                 }
 
